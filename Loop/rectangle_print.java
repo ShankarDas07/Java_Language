c292b86a619cd40a172b46ebc1498a0d23f3336d
Shankar Das <121621872+ShankarDas07@users.noreply.github.com>
2025-08-01 22:49:05 +0530

    Add files via upload

---
+++ Loop/rectangle_print.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+public class rectangle_print {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Raws : ");
+        int r=sc.nextInt();
+        System.out.print("Enter the Colomns ");
+        int c=sc.nextInt();
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=c;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
